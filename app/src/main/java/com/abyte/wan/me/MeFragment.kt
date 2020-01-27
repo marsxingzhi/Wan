8c3f1908720f79afd02f13ed5e56caebbbb3c248
geyan <2277087813@qq.com>
2020-01-27 22:53:36 +0800

    fix Me Part Tab

--- app/src/main/java/com/abyte/wan/me/MeFragment.kt
+++ app/src/main/java/com/abyte/wan/me/MeFragment.kt
@@ -1,9 +1,17 @@
 package com.abyte.wan.me
 
+import android.os.Bundle
+import android.view.View
 import com.abyte.wan.R
 import com.abyte.wan.core.base.ui.BaseFragment
+import com.abyte.wan.main.MainActivity
 
-class MeFragment :BaseFragment() {
+class MeFragment : BaseFragment() {
 
-    override fun getLayoutId(): Int  = R.layout.main_fragment_me
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).actionBarController.setupWithViewPager(null)
+    }
+
+    override fun getLayoutId(): Int = R.layout.main_fragment_me
 }
